5d701079ef24b40c1164da5e3980a886432f3937
Zhuangjinan <a492073467@gmail.com>
2021-07-20 14:54:14 +0800

    signup handler

--- account/handler/signup.go
+++ account/handler/signup.go
@@ -1,6 +1,10 @@
 package handler
 
 import (
+	"log"
+
+	"github.com/FuZhouJohn/memrizr/account/model"
+	"github.com/FuZhouJohn/memrizr/account/model/apperrors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +17,20 @@ func (h *Handler) Signup(c *gin.Context) {
 	var req signupReq
 
 	if ok := bindData(c, &req); !ok {
+		return
+	}
+
+	u := &model.User{
+		Email:    req.Email,
+		Password: req.Password,
+	}
+
+	err := h.UserService.Signup(c, u)
+	if err != nil {
+		log.Panicf("注册用户失败：%v\n", err.Error())
+		c.JSON(apperrors.Status(err), gin.H{
+			"error": err,
+		})
 		return
 	}
 }
