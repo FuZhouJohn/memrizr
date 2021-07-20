7569fcd1224d668d072d18fb5e08d33add6142eb
Zhuangjinan <a492073467@gmail.com>
2021-07-20 17:34:03 +0800

    token model and service

--- account/model/interface.go
+++ account/model/interface.go
@@ -11,6 +11,10 @@ type UserService interface {
 	Signup(ctx context.Context, u *User) error
 }
 
+type TokenService interface {
+	NewPairFromUser(ctx context.Context, u *User, prevTokenID string) (*TokenPair, error)
+}
+
 type UserRepository interface {
 	FindByID(ctx context.Context, uid uuid.UUID) (*User, error)
 }
